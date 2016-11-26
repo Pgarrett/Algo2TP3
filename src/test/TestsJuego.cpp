fe00b90fbd7fdbf5233982a36c5615aad37082f6
bernardo <btuso.95@gmail.com>
2016-11-26 15:42:09 -0300

    crear mapa default

--- src/test/TestsJuego.cpp
+++ src/test/TestsJuego.cpp
@@ -3,6 +3,7 @@
 #include "../modulos/Juego.h"
 
 typedef Coordenada Coor;
+Mapa crearMapaDefault();
 
 void TestsJuego::correr_tests() {
     // CrearJuego
@@ -21,20 +22,14 @@ void TestsJuego::correr_tests() {
 }
 
 void TestsJuego::test_juego_constructor_con_mapa() {
-    Mapa mapa;
-    mapa.agregarCoordenada(Coor(0, 0));
-    mapa.agregarCoordenada(Coor(0, 1));
-    mapa.agregarCoordenada(Coor(0, 2));
-    mapa.agregarCoordenada(Coor(1, 2));
-    mapa.agregarCoordenada(Coor(10, 0));
-    mapa.agregarCoordenada(Coor(1, 4));
+    Mapa mapa = crearMapaDefault();
 
     Juego j(mapa);
 
     ASSERT(j.mapa().coordenadas() == mapa.coordenadas());
 }
 
-void TestsJuego::test_juego_jugadores_con_id_secuencial() {
+Mapa crearMapaDefault(){
     Mapa mapa;
     mapa.agregarCoordenada(Coor(0, 0));
     mapa.agregarCoordenada(Coor(0, 1));
@@ -42,91 +37,68 @@ void TestsJuego::test_juego_jugadores_con_id_secuencial() {
     mapa.agregarCoordenada(Coor(1, 2));
     mapa.agregarCoordenada(Coor(10, 0));
     mapa.agregarCoordenada(Coor(1, 4));
+    return mapa;
+}
+
+void TestsJuego::test_juego_crear_juego_sin_jugadores() {
+    Mapa mapa = crearMapaDefault();
+
+    Juego j(mapa);
+
+    ASSERT(j.jugadores().Cardinal() == 0);
+}
+
+void TestsJuego::test_juego_jugadores_con_id_secuencial() {
+    Mapa mapa = crearMapaDefault();
 
     Juego j(mapa);
     ASSERT(j.agregarJugador() == 0);
     ASSERT(j.agregarJugador() == 1);
 }
 
-void TestsJuego::test_juego_jugador_nuevo_no_modifica_expulsados() {
-    Mapa mapa;
-    mapa.agregarCoordenada(Coor(0, 0));
-    mapa.agregarCoordenada(Coor(0, 1));
-    mapa.agregarCoordenada(Coor(0, 2));
-    mapa.agregarCoordenada(Coor(1, 2));
-    mapa.agregarCoordenada(Coor(10, 0));
-    mapa.agregarCoordenada(Coor(1, 4));
+void TestsJuego::test_juego_crear_juego_sin_expulsados() {
+    Mapa mapa = crearMapaDefault();
 
     Juego j(mapa);
+
+    ASSERT(j.expulsados().Cardinal() == 0);
+}
+
+void TestsJuego::test_juego_jugador_nuevo_no_modifica_expulsados() {
+    Mapa mapa = crearMapaDefault();
+
+    Juego j(mapa);
+
     ASSERT(j.expulsados().Cardinal() == 0);
     j.agregarJugador();
     ASSERT(j.expulsados().Cardinal() == 0);
 }
 
 void TestsJuego::test_juego_jugador_nuevo_desconectado() {
-    Mapa mapa;
-    mapa.agregarCoordenada(Coor(0, 0));
-    mapa.agregarCoordenada(Coor(0, 1));
-    mapa.agregarCoordenada(Coor(0, 2));
-    mapa.agregarCoordenada(Coor(1, 2));
-    mapa.agregarCoordenada(Coor(10, 0));
-    mapa.agregarCoordenada(Coor(1, 4));
-
+    Mapa mapa = crearMapaDefault();
     Juego j(mapa);
+
     Jugador jug = j.agregarJugador();
+
     ASSERT(not j.estaConectado(jug));
 }
 
 void TestsJuego::test_juego_jugador_nuevo_sin_sanciones() {
-    Mapa mapa;
-    mapa.agregarCoordenada(Coor(0, 0));
-    mapa.agregarCoordenada(Coor(0, 1));
-    mapa.agregarCoordenada(Coor(0, 2));
-    mapa.agregarCoordenada(Coor(1, 2));
-    mapa.agregarCoordenada(Coor(10, 0));
-    mapa.agregarCoordenada(Coor(1, 4));
-
+    Mapa mapa = crearMapaDefault();
     Juego j(mapa);
+
     Jugador jug = j.agregarJugador();
+
     ASSERT(j.sanciones(jug) == 0);
 }
 
 void TestsJuego::test_juego_jugador_nuevo_sin_pokemones() {
-    Mapa mapa;
-    mapa.agregarCoordenada(Coor(0, 0));
-    mapa.agregarCoordenada(Coor(0, 1));
-    mapa.agregarCoordenada(Coor(0, 2));
-    mapa.agregarCoordenada(Coor(1, 2));
-    mapa.agregarCoordenada(Coor(10, 0));
-    mapa.agregarCoordenada(Coor(1, 4));
-
+    Mapa mapa = crearMapaDefault();
     Juego j(mapa);
+
     Jugador jug = j.agregarJugador();
+
     ASSERT(j.sanciones(jug) == 0);
 }
 
-void TestsJuego::test_juego_crear_juego_sin_jugadores() {
-    Mapa mapa;
-    mapa.agregarCoordenada(Coor(0, 0));
-    mapa.agregarCoordenada(Coor(0, 1));
-    mapa.agregarCoordenada(Coor(0, 2));
-    mapa.agregarCoordenada(Coor(1, 2));
-    mapa.agregarCoordenada(Coor(10, 0));
-    mapa.agregarCoordenada(Coor(1, 4));
 
-    Juego j(mapa);
-    ASSERT(j.jugadores().Cardinal() == 0);
-}
-
-void TestsJuego::test_juego_crear_juego_sin_expulsados() {
-    Mapa mapa;
-    mapa.agregarCoordenada(Coor(0, 0));
-    mapa.agregarCoordenada(Coor(0, 1));
-    mapa.agregarCoordenada(Coor(0, 2));
-    mapa.agregarCoordenada(Coor(1, 2));
-    mapa.agregarCoordenada(Coor(10, 0));
-    mapa.agregarCoordenada(Coor(1, 4));
-
-    Juego j(mapa);
-    ASSERT(j.expulsados().Cardinal() == 0);
-}
